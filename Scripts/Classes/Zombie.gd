1d581743e05226862035097820f0e72b18a4bd67
copsfuraxx <jules.finck@hotmail.fr>
2022-04-03 11:41:07 +0200

    ZOMBIE - smart va jusqu'à la dernière position vue du joueur

--- Scripts/Classes/Zombie.gd
+++ Scripts/Classes/Zombie.gd
@@ -5,6 +5,14 @@ var cible = null
 var velocity_y := 0.0
 
 func _physics_process(delta):
+	_deplacement(delta)
+
+#		for i in range(0, get_slide_count()):
+#			var collider = get_slide_collision(i)
+#			if collider.collider.is_in_group("Joueur"):
+#				print("HURT")
+
+func _deplacement(delta:float):
 	if cible != null :
 		var space_state = get_world().direct_space_state
 		# use global coordinates, not local to node
@@ -15,21 +23,13 @@ func _physics_process(delta):
 			vec.y = $Vue.translation.y
 			$Vue.look_at(vec, Vector3.UP)
 
-			var direction = _deplacement()
+			var direction = translation.direction_to(cible.translation) * speed
 			if not is_on_floor():
 				velocity_y -= Global.gravity * delta
 				direction.y = velocity_y
 			else:
 				velocity_y = 0.0
 			move_and_slide(direction, Vector3.UP)
-
-#		for i in range(0, get_slide_count()):
-#			var collider = get_slide_collision(i)
-#			if collider.collider.is_in_group("Joueur"):
-#				print("HURT")
-
-func _deplacement():
-	return translation.direction_to(cible.translation) * speed
 
 func _on_Vue_body_entered(body):
 	cible = body
